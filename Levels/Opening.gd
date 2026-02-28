c279b97c5b2fe4963639cfc21fb136add5507e53
Tay6000 <Spike.hunterassing+github@gmail.com>
2026-02-28 12:05:50 -0500

    opening cutscene stuff might have happened

---
+++ Levels/Opening.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var Level_1 = preload("res://Levels/l_1.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Cutscene_sprites.play("default")
+	await get_tree().create_timer(1).timeout
+	_add_scene_manually()
+	get_node("/root/Open").free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _add_scene_manually():
+	get_tree().root.add_child(Level_1)
