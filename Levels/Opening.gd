addd59adcbd680111f6eebaf3e582facfd31a856
Tay6000 <Spike.hunterassing+github@gmail.com>
2026-02-28 12:44:21 -0500

    opening scene changes properly now

--- Levels/Opening.gd
+++ Levels/Opening.gd
@@ -6,7 +6,7 @@ var Level_1 = preload("res://Levels/l_1.tscn")
 func _ready() -> void:
 	$Cutscene_sprites.play("default")
 	await get_tree().create_timer(1).timeout
-	_add_scene_manually()
+	get_tree().root.add_child(Level_1.instantiate())
 	get_node("/root/Open").free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
